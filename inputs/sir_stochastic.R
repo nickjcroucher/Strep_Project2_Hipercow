f1054cd11c18c42b0c5efdf13af251ef1473f901
Nick Croucher <n.croucher@imperial.ac.uk>
2024-07-12 08:31:57 +0100

    Add births and deaths

--- inputs/sir_stochastic.R
+++ inputs/sir_stochastic.R
@@ -37,7 +37,7 @@ UK_calibration <- user(0.8066608) # FIXED (Lochen et al., 2022)
 log_delta <- user(0) # required in mcState
 sigma_1 <- user(1/15.75) # FIXED per-day, carriage duration (95% CI 7.88-31.49) (Serotype 1) (Chaguza et al., 2021)
 sigma_2 <- user(0)
-mu_0 <- user(0) # background mortality, assumed as closed system
+mu_0 <- 1/(80.70*365) # background mortality FIXED based on the inverse of life expectancy
 mu_1 <- user(192/(4064*4745)) # FIXED disease-associated mortality; ratio 192/4064 in 4745 days
 pi <- user(3.141593) # FIXED
 
@@ -65,34 +65,43 @@ wane <- 10^(log_wane)
 #wane <- 0
 
 # Individual probabilities of transition
-p_SA <- 1- exp(-lambda * dt)
-p_Asym <- 1- exp(-(delta+sigma_1) * dt)
+p_SA <- 1- exp(-(lambda+mu_0) * dt)
+p_Asym <- 1- exp(-(delta+mu_0+sigma_1) * dt)
 p_Dis <- 1- exp(-(sigma_2+mu_0+mu_1) * dt)
-p_RS <- 1- exp(-wane * dt)
+p_RS <- 1- exp(-(wane+mu_0) * dt)
 
 # Draws for numbers changing between compartments
 # Leaving S
-n_SA <- rbinom(S, p_SA)
+n_Suscep <- rbinom(S, p_SA)
+n_SA <- rbinom(n_Suscep, lambda/(lambda+mu_0))
+n_S_dead <- n_Suscep - n_SA
 
 # Leaving A
 n_Asym <- rbinom(A, p_Asym) # n_Asym <- n_AD + n_AR cause cyclic dependency error
-n_AD <- rbinom(n_Asym, delta/(delta+sigma_1))
-n_AR <- n_Asym - n_AD
+n_AD <- rbinom(n_Asym, delta/(delta+mu_0+sigma_1))
+n_AR <- rbinom((n_Asym - n_AD), sigma_1/(mu_0+sigma_1))
+n_A_dead <- n_Asym - n_AD - n_AR
 
 # Leaving D
 n_Dis <- rbinom(D, p_Dis)
 n_DR <- rbinom(n_Dis, sigma_2/(sigma_2+mu_0+mu_1))
-n_Dd <- n_Dis - n_DR
+n_Dd <- rbinom((n_Dis - n_DR), mu_1/(mu_0+mu_1))
+n_D_dead <- n_Dis - n_DR - n_Dd
 
 # Leaving R
-n_RS <- rbinom(R, p_RS)
+n_Resist <- rbinom(R, p_RS)
+n_RS <- rbinom(n_Resist, wane)
+n_R_dead <- n_Resist - n_RS
+
+# Closed system: births = deaths; all born susceptible
+n_S_born <- n_S_dead + n_Dd + n_D_dead + n_A_dead + n_R_dead
 
 # The transitions
 update(time) <- (step + 1) * dt
-update(S) <- S - n_SA + n_RS
-update(A) <- A + n_SA - (n_AD + n_AR)
-update(D) <- D + n_AD - (n_DR + n_Dd)
-update(R) <- R + n_AR + n_DR - n_RS
+update(S) <- S - n_SA + n_RS + n_S_born - n_S_dead
+update(A) <- A + n_SA - (n_AD + n_AR) - n_A_dead
+update(D) <- D + n_AD - (n_DR + n_Dd) - n_D_dead
+update(R) <- R + n_AR + n_DR - n_RS - n_R_dead
 # that "little trick" previously explained in https://github.com/mrc-ide/dust/blob/master/src/sir.cpp for cumulative incidence:
 # based on tutorial: https://mrc-ide.github.io/odin-dust-tutorial/mcstate.html#/the-model
 update(n_AD_daily) <- if (step %% freq == 0) n_AD else n_AD_daily + n_AD
